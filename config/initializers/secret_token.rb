0ee18706af08370c2d5977522c774ba274ce2a8b
Joan Roig <roigarderiu@gmail.com>
2013-05-06 18:23:43 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Menunyam::Application.config.secret_token = '598314dd903f95bc4d97c16532ef5ea9dbbf2daa5008954ff3c388b61e30569bb781c966ab842306698ebe0abedee6dc62049318c11aa294484865170fafc64b'
